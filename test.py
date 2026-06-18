033ebe17afb0c77a34f90d8eaae2bdc2a9a27cd0
Toufik333 <xbrtoufik@gmail.com>
2026-06-18 19:51:17 +0600

    Final Thesis

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 print("Hello, World!")
 
 import sys
-
+print("\n")
 # Prints a detailed string with build details
 print(sys.version)
+print("\n")
 
 # Returns a clean tuple format (e.g., major=3, minor=11, micro=2)
 print(sys.version_info)
