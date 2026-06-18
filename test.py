78ea5d4e134901f32340ef6d5642ab1fed10b6b8
Toufik333 <xbrtoufik@gmail.com>
2026-06-18 18:15:40 +0600

    after long time

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print("Hello, World!")
+
+import sys
+
+# Prints a detailed string with build details
+print(sys.version)
+
+# Returns a clean tuple format (e.g., major=3, minor=11, micro=2)
+print(sys.version_info)
